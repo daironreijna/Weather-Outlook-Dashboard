5be14e7c51c1d8837448f2696e321d8b252522bf
Dairon Reijna <dairon.reijna@gmail.com>
2023-09-16 21:12:46 +0100

    update index.css and script.js
    added in date formatting in script.js using dayjs()
    created an on click saving of user input to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,97 @@
-// API key
-// source: Stephen James (2019)
+/*$(document).ready(function () {
+*/
+  // latitude
+  const lat = 44.34;
+  // longitude
+  const lon = 10.99;
+  // Pulling secret API Key from config.js
+  const API_KEY = config.key;
 
-// latitude
-const lat = 44.34;
-// longitude
-const lon = 10.99;
-// Pulling secret API Key from config.js
-const API_KEY = config.key;
+  let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  let searchString = "";
+  let cityNumber = 0;
 
-let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  console.log(queryURL);
+  
+// the day of today 
+console.log(dayjs()); // everything as an object
 
-//console.log(queryURL);
+console.log(dayjs().$d); // example - Sat Sep 16 2023 18:02:19 GMT+0100 (British Summer Time)
 
-// use queryURL in order to get data from OpenWeather API
+console.log(dayjs().$d.getDate()); // the day e.g. today is 16.09.23 so expected output is 16
 
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.list[0].main.temp);
+todayYearMonthDate = dayjs().format('YYYY-MM-DD'); //2023-09-16
 
-    var todayDiv = $("#today");
+unixFormat = dayjs().unix();
 
-    todayDiv.text(`Temp: ${data.list[0].main.temp}  // °C Precipitation: ${data.list[0].pop} //Wind: ${data.list[0].wind.speed}KMPH 
+convertUnix = 
+console.log(todayYearMonthDate);
+console.log(unixFormat);
+console.log(convertUnix);
+
+let unix_timestamp = 1694887200
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+var date = new Date(unix_timestamp * 1000);
+
+console.log(date)
+  // use queryURL in order to get data from OpenWeather API
+
+  $(".search-button").on("click", function (event) {
+    //$("#article-results").empty();
+    event.preventDefault()
+    cityNumber += 1;
+    let searchString = $("#search-input").val();
+    localStorage.setItem(`cityNumber${cityNumber}`, searchString)
+    $("#search-input").empty();
+  });
+    /*
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // console.log(data);
+        //console.log(data.list[0].main.temp);
+
+        var todayDiv = $("#today");
+
+        // convert timestamps from unix to 
+
+        todayDiv.text(`Temp: ${data.list[0].main.temp}  °C  // Precipitation: ${data.list[0].pop} //Wind: ${data.list[0].wind.speed}KMPH 
     //
     Humidity: ${data.list[0].main.humidity}%`);
-    
-    console.log(data);
-    console.log(data.list[0].main.temp);
 
-    var todayDiv = $("#today");
+        //console.log(data);
+        //console.log(data.list[0].main.temp);
 
-    
-    // dayjs().format();
-    // var today = dayjs().format("YYYY-MM-DD");
-    // console.log(today);
+        var todayDiv = $("#today");
 
-    // var unixFormat = dayjs.unix(data.dt[0]).format("D MMM YYYY");
-    // console.log(unixFormat);
-    // var unixFormat2 = dayjs.unix(1694811600).format("D MMM YYYY");
-    // console.log(unixFormat2);
+
+        // dayjs().format();
+        // var today = dayjs().format("YYYY-MM-DD");
+        // console.log(today);
+
+        // var unixFormat = dayjs.unix(data.dt[0]).format("D MMM YYYY");
+        // console.log(unixFormat);
+        // var unixFormat2 = dayjs.unix(1694811600).format("D MMM YYYY");
+        // console.log(unixFormat2);
+      });
+    // https://www.w3schools.com/jsref/prop_node_textcontent.asp
+    // need to use id=today
+    //<section id="today" class="mt-3" role="region" aria-live="polite"></section>
+articleNumber++;
+                    var article = $("<div>");
+                    article.addClass("well well-lg row");
+                    var title = $("<h3>");
+                    title.addClass("title");
+                    title.text(data.articles[i].title);
+                    var description = $("<p>");
+                    description.addClass("description");
+                    description.text(data.articles[i].description);
+                    var number = $("<div class='articleNumber'>").text(articleNumber);
+                    $(article).append(number, title, description);
+                    $("#article-results").append(article);
+
   });
-// https://www.w3schools.com/jsref/prop_node_textcontent.asp
-// need to use id=today
-//<section id="today" class="mt-3" role="region" aria-live="polite"></section>
+});*/
